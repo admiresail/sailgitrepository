5e1878a91cd1907ebc5cbb65d10a5ddcb558b82c
Sailesh Mohan <admieme@gmail.com>
2019-01-14 12:14:41 +0530

    Jenkins demo for triggering the build from SCM
    Build trigger attempt 1.

--- Java project/Java file/HelloWorld.java
+++ Java project/Java file/HelloWorld.java
@@ -4,6 +4,6 @@
 public class HelloWorld
 {
 	public static void main(String[] args) {
-		System.out.println("Hello World - I am sailesh!");
+		System.out.println("Hello World - I am sailesh!! I am doing a demo on Hygieia");
 	}
 }
