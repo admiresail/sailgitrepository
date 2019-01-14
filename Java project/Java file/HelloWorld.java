810a1c68fbcffcc1d1618928f61fc39e3924569e
Sailesh Mohan <admieme@gmail.com>
2019-01-14 23:38:29 +0530

    Removed CD command.
    Removed CD command.

--- Java project/Java file/HelloWorld.java
+++ Java project/Java file/HelloWorld.java
@@ -4,6 +4,6 @@
 public class HelloWorld
 {
 	public static void main(String[] args) {
-		System.out.println("I am doing a demo on Hygieia.. This an independent java file");
+		System.out.println("Deleted the CD cmd !!!I am doing a demo on Hygieia.. This an independent java file");
 	}
 }
